d803bee5d3a2d3aed4ec0399533ee04ef5453e2f
greyglam <57892430+greyglam@users.noreply.github.com>
2022-09-30 11:14:40 +0700

    adding new book function

--- api/create.php
+++ api/create.php
@@ -7,12 +7,15 @@
 
     include_once '../config/database.php';
     include_once '../class/chapters.php';
+    include_once '../class/books.php';
 
     $database = new Database();
     $db = $database->getConnection();
 
     $item = new Chapter($db);
     $items = new Chapter($db);
+    $book = new Book($db);
+    $books = new Book($db);
 
     $data = json_decode(file_get_contents("php://input"));
     if ($data->chapter_title != '' &&  $data->chapter_content != '' && $data->book_name != '') {
@@ -20,8 +23,20 @@
         $item->chapter_content = $data->chapter_content;
         $item->chapter_no = $data->chapter_no;
         $item->book_name = $data->book_name;
+        
+        // CHeck book & insert book
+        $bookresult = $books->getBooksbyBookName($data->book_name);
+        $bookCount = $bookresult->rowCount();
+        if ($bookCount == 0){
+            $book->book_name =  $data->book_name;
+            $book->book_author =$data->book_author;
+            $book->createBook();
+        }
+
+        // Check chapter is existed
         $stmt = $items->getChaptersbyBookNameandChapter($data->book_name, $data->chapter_no);
         $itemCount = $stmt->rowCount();
+        // If chpater is not existed, insert to db
         if ($itemCount == 0) {
             if($item->createChapter()){
                 $result = '{"status": 200, "text":  "Chapter created successfully."}';
@@ -31,7 +46,9 @@
                 $result = '{"status": 400, "text":  "Chapter created fail."}';
                 echo $result;
             }
-        } else {
+        } 
+        // Return 400 if chapter existed
+        else {
             $result = '{"status": 400, "text":  "Chapter existed."}';
                 echo $result;
         }
--- class/books.php
+++ class/books.php
@@ -25,5 +25,28 @@ class Book
     $stmt->execute();
     return $stmt;
   }
+  public function getBooksbyBookName($bookName)
+  {
+    $sqlQuery = "SELECT id, book_name, book_author FROM " . $this->db_table . " WHERE book_name = '". $bookName . "'";
+    $stmt = $this->conn->prepare($sqlQuery);
+    $stmt->execute();
+    return $stmt;
+  }
 
+  public function createBook()
+  {
+    $sqlQuery = "INSERT INTO ".$this->db_table." SET book_name = :book_name, book_author = :book_author";
+    $stmt = $this->conn->prepare($sqlQuery);
+    // echo $sqlQuery;
+    // sanitize
+    $this->book_name = htmlspecialchars(strip_tags($this->book_name));
+    $this->book_author = htmlspecialchars(strip_tags($this->book_author));
+    // bind data
+    $stmt->bindParam(":book_name", $this->book_name);
+    $stmt->bindParam(":book_author", $this->book_author);
+    if ($stmt->execute()) {
+      return true;
+    }
+    return false;
+  }
 } ?>
